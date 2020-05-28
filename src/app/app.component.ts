242224efdfd4a89cdb8dc1faa7d236b9a823f5c2
Claude Mashinini <dumi.claude@gmail.com>
2020-05-28 11:33:05 +0200

    presentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'memory-game';
+
+  ngOnInit() { }
 }
